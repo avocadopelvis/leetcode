13637bc8e8c40e716e7a7ef93d2d8f468e0023b7
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-02-22 15:57:12 +0530

    Create 4. Level Averages in a Binary Tree.py

---
+++ Pattern 7: Tree Breadth First Search/4. Level Averages in a Binary Tree.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        result = []
+        if not root:
+            return result
+        
+        queue = deque()
+        queue.append(root)
+        
+        while queue:
+            levelSize = len(queue)
+            levelSum = 0.0 #take 0.0 to get decimal values later
+            
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+                #add the node's value to the running sum
+                levelSum += currentNode.val
+                #insert the children of current Node to the queue
+                if currentNode.left:
+                    queue.append(currentNode.left)    
+                if currentNode.right:
+                    queue.append(currentNode.right)
+            
+            #append the current level's average to result
+            result.append(levelSum/levelSize)
+            
+        return result
