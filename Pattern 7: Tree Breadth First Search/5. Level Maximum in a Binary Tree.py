c995795c3fbb8ffcc9a82b438ce0709f9f6aeba6
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-03-24 11:09:31 +0530

    Update 5. Level Maximum in a Binary Tree.py

--- Pattern 7: Tree Breadth First Search/5. Level Maximum in a Binary Tree.py
+++ Pattern 7: Tree Breadth First Search/5. Level Maximum in a Binary Tree.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+
 class Solution(object):
     def maxLevelSum(self, root):
         """
@@ -11,21 +13,24 @@ class Solution(object):
         queue.append(root)
         
         while queue:
+            level += 1
             levelSum = 0
             levelSize = len(queue)
             
             for _ in range(levelSize):
                 currentNode = queue.popleft()
-                
                 levelSum += currentNode.val
                 
+                # insert children of current node
                 if currentNode.left:
                     queue.append(currentNode.left)
                 if currentNode.right:
                     queue.append(currentNode.right)
-            
-            level += 1
-            
+             
             if levelSum > maxSum:
                 maxSum = levelSum
                 maxLevel = level
+
+        return maxLevel
+                
+            
