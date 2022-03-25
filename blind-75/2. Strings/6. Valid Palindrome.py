c72f02584486761a14e1b02303dff1149de6ec5d
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-03-25 10:01:17 +0530

    Update 6. Valid Palindrome.py

--- blind-75/2. Strings/6. Valid Palindrome.py
+++ blind-75/2. Strings/6. Valid Palindrome.py
@@ -1,3 +1,22 @@
+# https://leetcode.com/problems/valid-palindrome/
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+
+# First, we have to make sure to remove all non-alphanumeric characters & all characters are lowercase
+
+# Example of palindrome: radar 
+
+# • Use two pointers
+#   left pointer in the beginning & right in the end
+
+# • Make sure the characters are alphanumeric using isalnum()
+
+# • Iterate & Compare values of the pointers
+#   If all values are equal then it is a valid palindrome 
+
+# Alternate solution:
+# It is shorter but uses extra memory because of new list
+
 class Solution(object):
     def isPalindrome(self, s):
         """
