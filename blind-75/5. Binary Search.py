fd8000bff56cd8f2591d41908ca1f040ad56f582
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-05-22 11:23:26 +0530

    Create 5. Binary Search.py

---
+++ blind-75/5. Binary Search.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/binary-search/solution/
+
+# look at solution in leetcode for explanation
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        
+        while l <= r:
+            mid = l + (r-l)//2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return -1
