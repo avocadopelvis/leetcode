6b5b22a3964389c15c84015e4e1fb0568285fb74
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-03 13:50:30 +0530

    Update 2. Find All Numbers Disappeared in an Array.py

--- Pattern 5: Cyclic Sort/2. Find All Numbers Disappeared in an Array.py
+++ Pattern 5: Cyclic Sort/2. Find All Numbers Disappeared in an Array.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 # SET SUBTRACTION
-# take a set with numbers from the size of the array nums and subtract it with set containing the values in the array nums
+# take a set with numbers in the range from the size of the array nums and subtract it with set containing the values in the array nums
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         
