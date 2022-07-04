63192812f741c9de53e164018f7bcb28d0e40464
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-05 01:53:56 +0530

    Update 1. Missing Number.py

--- Pattern 5: Cyclic Sort/1. Missing Number.py
+++ Pattern 5: Cyclic Sort/1. Missing Number.py
@@ -1,2 +1,10 @@
 # https://leetcode.com/problems/missing-number/
 
+# set subtraction 
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        s = set(range(len(nums)+1)) - set(nums)
+        return s.pop()
+      
+      
