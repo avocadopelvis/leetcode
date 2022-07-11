3560c10089ce0094b06e45278e7b35b88fb78deb
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-12 03:20:03 +0530

    Create 607. Sales Person.sql

---
+++ SQL/607. Sales Person.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/sales-person/
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id  NOT IN
+(
+    SELECT sales_id
+    FROM Orders o JOIN Company c
+    ON o.com_id = c.com_id
+    WHERE c.name = "RED"
+);    
