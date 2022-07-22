82d7c1fad09665b99fc8876da0744848f2aed061
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-22 11:09:36 +0530

    Update 7. Find Minimum in Rotated Sorted Array.py

--- blind-75/1. Array/7. Find Minimum in Rotated Sorted Array.py
+++ blind-75/1. Array/7. Find Minimum in Rotated Sorted Array.py
@@ -1,1 +1,32 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+# Explanation
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/discuss/158940/Beat-100%3A-Very-Simple-(Python)-Very-Detailed-Explanation
+# checkout neetcode solution again
+
+# requires editing
+
+# right sorted portion will have values smaller than left sorted portion
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        
+        # DO NOT use left <= right because that would loop forever
+        while l < r:
+            mid = l + (r-l)//2
+            
+            # if this holds true then mid is in right sorted portion
+            if nums[mid] > nums[r]:
+                # we want to be in left sorted portion since smaller numbers are over there
+                l = mid + 1
+
+            # mid is in left sorted portion
+            else: # nums[mid] <= nums[right]
+                # the right side of mid will have numbers greater than mid
+                # so we search in the left side of mid
+                r = mid
+                
+        return nums[l]
+      
+      
