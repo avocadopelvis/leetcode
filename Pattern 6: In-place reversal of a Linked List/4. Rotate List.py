e9fc77e7fd585c04cb9ac70d71781f77740af6fa
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-24 17:17:36 +0530

    Create 4. Rotate List.py

---
+++ Pattern 6: In-place reversal of a Linked List/4. Rotate List.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/rotate-list/
+
+
+# The time complexity of our algorithm will be O(N) where N is the total number of nodes in the LinkedList.
+# We only used constant space, therefore, the space complexity of our algorithm is O(1).
