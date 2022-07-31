3e678cb7eef32b08d3174dc494e8d075a9a005b9
elvis <92647313+avocadopelvis@users.noreply.github.com>
2022-07-31 17:07:03 +0530

    Create 5. Subarray Product Less Than K.py

---
+++ Pattern 2: Two Pointers/5. Subarray Product Less Than K.py
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/subarray-product-less-than-k/
+
+# https://leetcode.com/problems/subarray-product-less-than-k/discuss/560093/Python3-two-pointer-O(N)-O(1)-with-breakdown
